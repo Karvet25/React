327acebcfda99c861c8463060d2f4ed64a0c899e
TiamatBronze1 <vukolovnikita25052005@gmail.com>
2023-11-03 10:17:54 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 // Компонент для представления отдельной задачи
@@ -6,18 +6,26 @@ const TaskItem = ({ task, onToggle, onDelete, onEdit }) => {
   const { id, text, completed } = task;
 
   return (
-    <div>
-      <span
+    <div className="task-cont">
+      <div
+        className="task-text"
         style={{ textDecoration: completed ? 'line-through' : 'none' }}
         onClick={() => onToggle(id)}
       >
         {text}
-      </span>
-      <button className="glow-on-hover1" onClick={() => onDelete(id)}>Удалить</button>
-      <button className={`glow-on-hover ${completed ? 'completed' : ''}`} onClick={() => onEdit(id)}>Редактировать</button>
+      </div>
+      <div className="task-buttons">
+        <button className="glow-on-hover1" onClick={() => onDelete(id)}>Удалить</button>
+      </div>
+      <div className="task-buttons1">
+        <button className={`glow-on-hover ${completed ? 'completed' : ''}`} onClick={() => onEdit(id)}>Редактировать</button>
+      </div>
     </div>
   );
 };
+
+
+
 
 // Компонент для отображения списка задач
 const TaskList = ({ tasks, onToggle, onDelete, onEdit, filterStatus }) => {
@@ -27,7 +35,7 @@ const TaskList = ({ tasks, onToggle, onDelete, onEdit, filterStatus }) => {
   return (
     <div>
       {filteredTasks.map((task) => (
-        <div key={task.id} className="task-container">
+        <div key={task.id} className="task-contain">
           <TaskItem
             task={task}
             onToggle={onToggle}
@@ -55,25 +63,58 @@ const TaskForm = ({ onSubmit, taskToEdit }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
       <input
         type="text"
         placeholder="Введите задачу"
         value={text}
         onChange={(e) => setText(e.target.value)}
+        style={{
+          width: '90%',
+          padding: '10px',
+          borderRadius: '10px', // Добавьте закругление краев
+        }} 
       />
-      <button className="glow-on-hover2"  type="submit">{taskToEdit ? 'Редактировать' : 'Добавить'}</button>
+      <button className="glow-on-hover2" type="submit" 
+        style={{
+          padding: '18px',
+          textAlign: 'center',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {taskToEdit ? 'Редактировать' : 'Добавить'}
+      </button>
     </form>
   );
 };
 
 export default function App() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem('tasks')) || []
+  );
   const [taskToEdit, setTaskToEdit] = useState(null);
-  const [filterStatus, setFilterStatus] = useState("all"); // По умолчанию показываем все задачи
+  const [filterStatus, setFilterStatus] = useState("all");
+
+  // Эффект для загрузки задач из локального хранилища при старте
+  useEffect(() => {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    }
+  }, []);
+
+  // Эффект для сохранения задач в локальном хранилище, когда они изменяются
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+
 
   const addTask = (newTask) => {
-    setTasks([...tasks, { ...newTask, id: tasks.length + 1, completed: false }]);
+    const newTaskWithId = { ...newTask, id: Date.now(), completed: false };
+    setTasks([...tasks, newTaskWithId]);
   };
 
   const toggleTask = (taskId) => {
@@ -103,18 +144,20 @@ export default function App() {
   };
 
   return (
-    <div>
+    <div className="container">
       <h1>Список задач</h1>
-      <div>
+      <div class="qq">
         <button class="qwerty" onClick={() => setFilterStatus("all")}>Все</button>
-        <button class="qwerty" onClick={() => setFilterStatus("completed")}>Завершенные</button>
-        <button class="qwerty" onClick={() => setFilterStatus("uncompleted")}>Незавершенные</button>
+        <button class="qwerty1" onClick={() => setFilterStatus("completed")}>Завершенные</button>
+        <button class="qwerty1" onClick={() => setFilterStatus("uncompleted")}>Незавершенные</button>
       </div>
       <TaskForm onSubmit={taskToEdit ? updateTask : addTask} taskToEdit={taskToEdit} />
       <TaskList tasks={tasks} onToggle={toggleTask} onDelete={deleteTask} onEdit={editTask} filterStatus={filterStatus} />
     </div>
   );
 }
+
+
 
 
 
---
+++ src/Setting.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Компонент для страницы настроек
+function Setting() {
+  return (
+    <div>
+      <h2>Настройки</h2>
+      {/* Здесь можно разместить компоненты и формы для редактирования настроек */}
+    </div>
+  );
+}
+
+export default Setting;
